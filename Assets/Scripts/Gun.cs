38b948e7b2b9664934f9e44484aff621e5c4c879
Ryan Dostalek <rdostale@calpoly.edu>
2025-02-28 14:32:57 -0800

    Added: Barbones shooting logic

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    // Fire rate in bullets per second
+    public float fireRate = 1f;
+    public float bulletSpeed = 10f;
+    public float bulletDamage = 1f;
+    public int magSize = 15;
+    public float reloadTimeSeconds = 1f;
+    public Transform firePoint;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Fire()
+    {
+
+    }
+
+    public void Reload()
+    {
+
+    }
+}
