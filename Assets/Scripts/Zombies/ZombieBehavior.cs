490623ed01d9e997981baaf9146648c96080793b
Baker-C <cdbaker.dev@gmail.com>
2025-03-01 13:40:42 -0800

    Zombies !!!
    Added a basic zombie prefab.
    Created a zombie manager for spawn rates & positions.

---
+++ Assets/Scripts/Zombies/ZombieBehavior.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using System.Collections;
+
+public class ZombieBehavior : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] Transform target;
+
+    [Header("Attributes")]
+    [SerializeField] float attackRange = 2f;
+    [SerializeField] float attackDamage = 10f;
+    [SerializeField] float attackLength = 0.3f;
+    [SerializeField] float attackDelay = 1f;
+    [SerializeField] float movementSpeed = 1f;
+
+    bool isAttacking = false;
+    float distance = 0f;
+    BoxCollider2D attackCollider;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (target == null)
+            target = GameObject.FindWithTag("Player").transform;
+        
+        attackCollider = gameObject.GetComponent<BoxCollider2D>();
+        attackCollider.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isAttacking)
+            return;
+
+        if (targetInRange())
+            Attack();
+        else
+            Move();
+    }
+    
+    bool targetInRange()
+    {
+        distance = Vector3.Distance(transform.position, target.position);
+        return distance <= attackRange;
+    }
+
+    void Attack()
+    {
+        Debug.Log("Zombie attacked the player");
+        StartCoroutine(AttackRoutine());
+    }
+
+    IEnumerator AttackRoutine()
+    {
+        isAttacking = true;
+        attackCollider.enabled = true;
+        yield return new WaitForSeconds(attackLength);
+        attackCollider.enabled = false;
+        yield return new WaitForSeconds(attackDelay);
+        isAttacking = false;
+    }
+
+    void Move()
+    {
+        Debug.Log("Zombie is moving");
+        Vector3 direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        transform.position = Vector3.MoveTowards(transform.position, target.position, movementSpeed * Time.fixedDeltaTime);
+    }
+}
