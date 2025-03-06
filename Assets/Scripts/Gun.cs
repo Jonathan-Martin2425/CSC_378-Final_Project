318fc840123b3c7433530282fe04c8719d86b80a
Jonathan-Martin2425 <jonathan.martin2425@gmail.com>
2025-03-05 16:53:00 -0800

    Milestone 2 final

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -31,6 +31,7 @@ public class Gun : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        currentAmmo = magSize;
     }
 
     // Update is called once per frame
