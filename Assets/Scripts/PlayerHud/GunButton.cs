7a4156aead62cc49f7d7057e9421dcdc22fda3f9
ryanvuu <ryanvu077@gmail.com>
2025-03-12 18:13:23 -0700

    bug fix weapon select by ui

--- Assets/Scripts/PlayerHud/GunButton.cs
+++ Assets/Scripts/PlayerHud/GunButton.cs
@@ -7,7 +7,7 @@ public class GunButton : MonoBehaviour
     public GameObject player;
     public Gun gun;
 
-    public void GunSelected()
+    public void SelectGun()
     {
         player.GetComponent<PlayerWeaponController>().SwitchGun(gun.id);
     }
