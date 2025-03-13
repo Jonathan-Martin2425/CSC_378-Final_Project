f170b523cfa7dd533ccd5eaf4b7e33c1c3e294d6
ryanvuu <ryanvu077@gmail.com>
2025-03-12 23:15:32 -0700

    add reload icon

--- Assets/Scripts/PlayerWeaponController.cs
+++ Assets/Scripts/PlayerWeaponController.cs
@@ -117,14 +117,12 @@ public class PlayerWeaponController : MonoBehaviour
 
             if (onCooldown)
             {   
-                Debug.Log($"Adding overlay for {weaponSlots[i].name}");
                 GameObject tempOverlay = Instantiate(swapOverlay, weaponSlots[i].transform);
                 tempOverlay.name = "SwapOverlay";
                 tempOverlay.GetComponent<RectTransform>().localScale = weaponSlots[i].GetComponent<RectTransform>().sizeDelta;
             }
             else
             {
-                Debug.Log($"Removing overlay for {weaponSlots[i].name}");
                 Transform toRemove = weaponSlots[i].transform.Find("SwapOverlay");
                 Destroy(toRemove.gameObject);
             }
