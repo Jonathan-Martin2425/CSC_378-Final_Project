c7d3c48fd6168384fbd6402eda9c88090104f8ef
Ryan Dostalek <rdostale@calpoly.edu>
2025-03-16 17:40:44 -0700

    Changed bullet collider to trigger

--- Assets/Scripts/PlayerHud/InfoUpgradePanel.cs
+++ Assets/Scripts/PlayerHud/InfoUpgradePanel.cs
@@ -137,12 +137,20 @@ public class InfoPanel : MonoBehaviour
         switch(level)
         {
             case 0:
+                sniper.fireRate = 1.5f;
                 levels[currentWeapon.id] += 1;
-                currentWeapon.level += 1;
+                sniper.level += 1;
+
+                break;
+            case 1:
+                sniper.pierce = 3;
+                levels[currentWeapon.id] += 1;
+                sniper.level += 1;
 
                 break;
             default:
                 Debug.Log("Sniper fully upgraded");
+
                 break;
         }
     }
